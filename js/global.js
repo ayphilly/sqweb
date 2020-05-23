58c46bb28d65f391e5373860dcaa1a5da1377976
adewale ayomiposi <phil.a@softquestsystems.com>
2020-05-23 02:14:29 +0100

    services

--- js/global.js
+++ js/global.js
@@ -1,5 +1,17 @@
 // Global Javascript Initialization
-
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
 var Global = function() {
   'use strict';
 
