2a3af9136eb4fcffdeccfa07bf91018542d4cdae
adewale ayomiposi <phil.a@softquestsystems.com>
2020-05-23 17:04:53 +0100

    icons

--- js/global.js
+++ js/global.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
  
   $("#owl-demo").owlCarousel({
  
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      autoPlay: 2000, //Set AutoPlay to 3 seconds
  
       items : 3,
       itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
+      itemsDesktopSmall : [1024,1]
  
   });
  
